b0aa427a90340fb5c718a76e93e1d7f898932832
imronshoimov <imronshoimov125@gmail.com>
2021-04-26 03:57:38 +0500

    Create card options

---
+++ main.js
@@ -0,0 +1,51 @@
+// card options
+const cardArray = [
+    {
+        name: 'cheesburger',
+        img: './images/cheesburger.jpg'
+    },
+    {
+        name: 'cheesburger',
+        img: './images/cheesburger.jpg'
+    },
+    {
+        name: 'fries',
+        img: './images/fries.jpg'
+    },
+    {
+        name: 'fries',
+        img: './images/fries.jpg'
+    },
+    {
+        name: 'hotdog',
+        img: './images/hotdog.webp'
+    },
+    {
+        name: 'hotdog',
+        img: './images/hotdog.webp'
+    },
+    {
+        name: 'ice-cream',
+        img: './images/ice-cream.webp'
+    },
+    {
+        name: 'ice-cream',
+        img: './images/ice-cream.webp'
+    },
+    {
+        name: 'milkshake',
+        img: './images/milkshake.jpg'
+    },
+    {
+        name: 'milkshake',
+        img: './images/milkshake.jpg'
+    },
+    {
+        name: 'pizza',
+        img: './images/pizza.jpeg'
+    },
+    {
+        name: 'pizza',
+        img: './images/pizza.jpeg'
+    }
+]
